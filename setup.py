a8d35ef1cff53934877a2bbd0293b89ca22dcc7e
comprende-prod <rent@comprende.co.nz>
2023-09-22 15:03:23 +1200

    Removed old files

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="trademe",
+    version="0.1.0",
+    author="Ethan Beri",
+    author_email="ethandberi01@gmail.com",
+    description="Scrapes TradeMe residential properties.",
+    url="https://github.com/comprende-prod/trademe",
+    license="GNU GPLv3",
+    packages=["trademe"],
+    install_requires=["selenium", "bs4", "urllib", "dataclasses"]
+)
+
